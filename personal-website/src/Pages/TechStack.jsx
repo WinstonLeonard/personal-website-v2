28e5fb4b63a53fc3f77676fbccfc26adae512890
Winston <leonardwinstoncx9@gmail.com>
2025-06-22 13:18:53 +0800

    Tech Stack mobile

--- personal-website/src/Pages/TechStack.jsx
+++ personal-website/src/Pages/TechStack.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, memo, useState, useCallback } from "react";
-import { Laptop, MonitorCog, BrainCircuit, Code, Wrench, Github } from "lucide-react";
-import { Button } from '@mui/material';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import {
+  Laptop,
+  MonitorCog,
+  BrainCircuit,
+  Code,
+  Wrench,
+  Github,
+} from "lucide-react";
+import { Button } from "@mui/material";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const AnimatedHeader = () => (
-  <div className="mt-60 text-center lg:mb-8 mb-2 px-[5%]">
-    <div className="inline-block relative group" data-aos="zoom-in-up" data-aos-duration="600">
+  <div className="mt-40 text-center lg:mb-8 mb-2 px-[5%]">
+    <div
+      className="inline-block relative group"
+      data-aos="zoom-in-up"
+      data-aos-duration="600"
+    >
       <h2 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#6366f1] to-[#a855f7]">
         Tech Stack
       </h2>
@@ -14,27 +25,29 @@ const AnimatedHeader = () => (
   </div>
 );
 
-const SelectionChoice = memo(({ text, icon, onClick, currentSelected }) => (
-    currentSelected == text ? 
-    (
-          <div
-    className="group bg-[#362A54] flex-grow rounded-xl flex flex-col items-center justify-center hover:bg-[#362A54] transition-all duration-300 cursor-pointer" 
-    onClick={() => onClick(text)}
-  >
-    {icon && <div className="text-white">{icon}</div>}
-    <p className="text-white">{text}</p>
-  </div>
-    ) :
-    (
-  <div
-    className="group flex-grow rounded-xl flex flex-col items-center justify-center hover:bg-[#271F3D] hover:scale-105 transition-all duration-300 cursor-pointer" 
-    onClick={() => onClick(text)}
-  >
-    {icon && <div className="text-gray-400 group-hover:text-white group-hover:rotate-4 duration-300">{icon}</div>}
-    <p className="text-gray-400 group-hover:text-white">{text}</p>
-  </div>
-    )
-));
+const SelectionChoice = memo(({ text, icon, onClick, currentSelected }) =>
+  currentSelected == text ? (
+    <div
+      className="group bg-[#362A54] flex-grow rounded-xl flex flex-col items-center justify-center hover:bg-[#362A54] transition-all duration-300 cursor-pointer"
+      onClick={() => onClick(text)}
+    >
+      {icon && <div className="text-white">{icon}</div>}
+      <p className="text-white">{text}</p>
+    </div>
+  ) : (
+    <div
+      className="group flex-grow rounded-xl flex flex-col items-center justify-center hover:bg-[#271F3D] hover:scale-105 transition-all duration-300 cursor-pointer"
+      onClick={() => onClick(text)}
+    >
+      {icon && (
+        <div className="text-gray-400 group-hover:text-white group-hover:rotate-4 duration-300">
+          {icon}
+        </div>
+      )}
+      <p className="text-gray-400 group-hover:text-white">{text}</p>
+    </div>
+  )
+);
 
 const AnimatedSelectionBar = ({ onSelect, currentSelected }) => (
   <div className="self-center justify-self-center mt-10 w-[85%]">
@@ -42,71 +55,232 @@ const AnimatedSelectionBar = ({ onSelect, currentSelected }) => (
       <div className="rounded-xl bg-gradient-to-r from-[#2B282B] to-[#242424] backdrop-blur-xl h-26 relative p-2">
         <div className="absolute inset-0 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-xl blur opacity-20 transition duration-300"></div>
         <div className="h-full rounded-xl bg-black/50 backdrop-blur-xl flex space-x-4 py">
-          <SelectionChoice text="Frontend" icon={<Laptop />} onClick={onSelect} currentSelected={currentSelected} />
-          <SelectionChoice text="Backend" icon={<MonitorCog />} onClick={onSelect} currentSelected={currentSelected}/>
-          <SelectionChoice text="AI / ML" icon={<BrainCircuit />} onClick={onSelect} currentSelected={currentSelected}/>
-          <SelectionChoice text="Languages" icon={<Code />} onClick={onSelect} currentSelected={currentSelected} />
-          <SelectionChoice text="Tools" icon={<Wrench />} onClick={onSelect} currentSelected={currentSelected} />
+          <SelectionChoice
+            text="Frontend"
+            icon={<Laptop />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoice
+            text="Backend"
+            icon={<MonitorCog />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoice
+            text="AI / ML"
+            icon={<BrainCircuit />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoice
+            text="Languages"
+            icon={<Code />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoice
+            text="Tools"
+            icon={<Wrench />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
         </div>
       </div>
     </div>
   </div>
 );
 
-const Icons = () => (
-  <button className="cursor-pointer group relative p-3 transform transition-all duration-300 hover:scale-105 hover:shadow-lg mr-8">
-    <div className="absolute inset-0 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-xl blur opacity-20 group-hover:opacity-40 transition duration-300"></div>
-    <div className="relative rounded-xl bg-black/50 backdrop-blur-xl p-2 flex flex-col items-center justify-center border border-white/10 group-hover:border-white/20 transition-all duration-300 w-40 h-40">
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-        className="w-20 h-20 object-contain"
-      />
-      <p className="text-xl mt-2 font-semibold">React</p>
+const SelectionChoiceMobile = memo(({ text, icon, onClick, currentSelected }) =>
+  currentSelected == text ? (
+    <div
+      className="group bg-[#362A54] rounded-xl flex flex-col items-center justify-center hover:bg-[#362A54] transition-all duration-300 cursor-pointer flex-wrap flex-grow mx-4"
+      onClick={() => onClick(text)}
+    >
+      {icon && <div className="text-white">{icon}</div>}
+      <p className="text-white">{text}</p>
+    </div>
+  ) : (
+    <div
+      className="group rounded-xl flex flex-col items-center justify-center hover:bg-[#271F3D] hover:scale-105 transition-all duration-300 cursor-pointer flex-wrap flex-grow mx-4"
+      onClick={() => onClick(text)}
+    >
+      {icon && (
+        <div className="text-gray-400 group-hover:text-white group-hover:rotate-4 duration-300">
+          {icon}
+        </div>
+      )}
+      <p className="text-gray-400 group-hover:text-white">{text}</p>
+    </div>
+  )
+);
+
+const AnimatedSelectionBarMobile = ({ onSelect, currentSelected }) => (
+  <div className="self-center justify-self-center mt-10 w-[90%]">
+    <div data-aos="zoom-in-up" data-aos-duration="600">
+      <div className="rounded-xl bg-gradient-to-r from-[#2B282B] to-[#242424] backdrop-blur-xl min-h-20 relative p-1">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-xl blur opacity-20 transition duration-300"></div>
+        <div className="h-full rounded-xl bg-black/50 backdrop-blur-xl flex space-x-4 flex-wrap justify-center items-center p-4">
+          <SelectionChoiceMobile
+            text="Frontend"
+            icon={<Laptop />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoiceMobile
+            text="Backend"
+            icon={<MonitorCog />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoiceMobile
+            text="AI / ML"
+            icon={<BrainCircuit />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoiceMobile
+            text="Languages"
+            icon={<Code />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+          <SelectionChoiceMobile
+            text="Tools"
+            icon={<Wrench />}
+            onClick={onSelect}
+            currentSelected={currentSelected}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const Icons = ({ name, url }) => (
+  <button className="cursor-pointer group relative p-3 transform transition-all duration-300 hover:scale-105 hover:shadow-lg mr-8 mt-3">
+    <div data-aos="zoom-in-up" data-aos-duration="600">
+      <div className="absolute inset-0 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-xl blur opacity-20 group-hover:opacity-40 transition duration-300"></div>
+      <div className="relative rounded-xl bg-black/50 backdrop-blur-xl p-2 flex flex-col items-center justify-center border border-white/10 group-hover:border-white/20 transition-all duration-300 w-35 h-35">
+        <img src={url} className="w-19 h-19 object-contain" />
+        <p className="text-xl mt-2 font-semibold">{name}</p>
+      </div>
     </div>
   </button>
 );
 
+const IconContainer = ({ selectedIcons }) => (
+  <div className="flex flex-wrap self-center justify-self-center mt-10 w-[85%] p-5 justify-center">
+    {selectedIcons.map((icon, index) => (
+      <Icons key={index} name={icon.name} url={icon.url} />
+    ))}
+  </div>
+);
 
-const IconContainer = () => (
-    <div className="flex flex-wrap self-center justify-self-center mt-10 w-[85%] p-5 justify-center space-y-5">
-        <Icons />
-                <Icons />
-
-        <Icons />
-
-        <Icons />
-
-        <Icons />
-
-        <Icons />
-
-        <Icons />
-
-
+const IconsMobile = ({ name, url }) => (
+  <button className="cursor-pointer group relative p-2 transform transition-all duration-300 hover:scale-105 hover:shadow-lg mt-3">
+    <div data-aos="zoom-in-up" data-aos-duration="600">
+      <div className="absolute inset-0 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-xl blur opacity-20 group-hover:opacity-40 transition duration-300"></div>
+      <div className="relative rounded-xl bg-black/50 backdrop-blur-xl p-2 flex flex-col items-center justify-center border border-white/10 group-hover:border-white/20 transition-all duration-300 w-20 h-20">
+        <img src={url} className="w-8 h-8 object-contain" />
+        <p className="text-sm mt-2 font-semibold">{name}</p>
+      </div>
     </div>
-)
+  </button>
+);
+
+const IconContainerMobile = ({ selectedIcons }) => (
+  <div className="flex flex-wrap self-center justify-self-center mt-10 w-[85%] justify-center">
+    {selectedIcons.map((icon, index) => (
+      <IconsMobile key={index} name={icon.name} url={icon.url} />
+    ))}
+  </div>
+);
 
 function TechStack() {
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState("Frontend");
+  const [frontEndData, setFrontEndData] = useState([
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+    {
+      name: "React",
+      url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    },
+  ]);
+  const [backEndData, setBackEndData] = useState([]);
+  const [aiMlData, setAiMlData] = useState([]);
+  const [languagesData, setLanguagesData] = useState([]);
+  const [toolsData, setToolsData] = useState([]);
+
+  const [selectedIcons, setSelectedIcons] = useState(frontEndData);
 
   const handleSelection = useCallback((text) => {
     setSelected(text);
+    switch (text) {
+      case "Frontend":
+        setSelectedIcons(frontEndData);
+        break;
+      case "Backend":
+        setSelectedIcons(backEndData);
+        break;
+      case "AI / ML":
+        setSelectedIcons(aiMlData);
+        break;
+      case "Languages":
+        setSelectedIcons(languagesData);
+        break;
+      case "Tools":
+        setSelectedIcons(toolsData);
+        break;
+      default:
+        setSelectedIcons([]);
+    }
   }, []);
 
   useEffect(() => {
     AOS.init({
-        mirror: true,
+      mirror: true,
     });
 
     return () => AOS.refreshHard();
   }, []);
 
   return (
-    <div
-          id="TechStack">
-      <AnimatedHeader />
-      <AnimatedSelectionBar onSelect={handleSelection} currentSelected={selected} />
-      <IconContainer />
+    <div id="TechStack">
+      <div className="hidden md:flex flex-col">
+        <AnimatedHeader />
+        <AnimatedSelectionBar
+          onSelect={handleSelection}
+          currentSelected={selected}
+        />
+        <IconContainer selectedIcons={selectedIcons} />
+      </div>
+      <div class="flex md:hidden flex-col">
+        <AnimatedHeader />
+        <AnimatedSelectionBarMobile
+          onSelect={handleSelection}
+          currentSelected={selected}
+        />
+        <IconContainerMobile selectedIcons={selectedIcons} />
+      </div>
     </div>
   );
 }
