027aac430033b4ddbcdbe62be829ca012955a086
Winston <leonardwinstoncx9@gmail.com>
2025-09-07 02:45:58 +0800

    Update Certificates.jsx

--- personal-website/src/Pages/Certificates.jsx
+++ personal-website/src/Pages/Certificates.jsx
@@ -1,5 +1,11 @@
 import AnimatedHeader from "../components/AnimatedHeader";
 import React, { useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 const CertificateCard = ({ certificate }) => {
   const [showModal, setShowModal] = useState(false);
@@ -15,7 +21,15 @@ const CertificateCard = ({ certificate }) => {
           <div className="flex-grow absolute inset-0 bg-gradient-to-br from-blue-500/10 via-purple-500/10 to-pink-500/10 opacity-50 group-hover:opacity-70 transition-opacity duration-300"></div>
           <div className="flex flex-grow relative flex-col w-full h-65 md:h-80 relative p-2">
             <div className="w-full h-[85%] flex items-center justify-center overflow-hidden rounded-xl bg-gray-900">
-              <span className="text-5xl text-purple-300">📄</span>
+              <div
+                className="absolute inset-0 bg-transparent z-10 overflow-hidden cursor-pointer"
+                onClick={() => setShowModal(true)} // Open the modal when clicked
+              ></div>
+              <iframe
+                src={certificate.pdfUrl + "#toolbar=0"}
+                title="Certificate Preview"
+                className="w-full h-full rounded-lg border no-scrollbar overflow-hidden"
+              ></iframe>
             </div>
             <div className="items-start justify-start text-left group p-2">
               <p className="text-lg font-semibold bg-gradient-to-r from-blue-200 via-white-200 to-purple-200 bg-clip-text text-transparent">
